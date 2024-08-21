8064aedfd730cd6caa19f81864d47a4c03871e94
Harsh Vardhan <harshvardhan232002@gmail.com>
2024-08-21 14:57:29 +0530

    js

---
+++ index.js
@@ -0,0 +1,42 @@
+let a=7;
+let b=3;
+var sum=a+b;
+console.log("sum is = "+sum)
+
+let c=15;
+let d=8;
+var difference=c-d;
+console.log("diff. is = "+difference)
+
+let e=6;
+let f=4;
+var product=e*f;
+console.log("product. is = "+product)
+
+let g=20;
+let h=4;
+var quotient=g/h;
+console.log("quotient. is = "+quotient)
+
+let i=10;
+let j=3;
+var remainder=i%j;
+console.log("remainder. is = "+remainder)
+
+let k=2;
+let l=3;
+let m=4;
+var js=(k+l)*m;
+console.log("Answer is = "+js)
+
+let n=5;
+let o=10;
+let p=15;
+var avg=(n+o+p)/3;
+console.log("Average is = "+avg)
+
+let q=7;
+let r=5;
+++q;
+var jsk=(q)*r;
+console.log("Answer. is = "+jsk)
